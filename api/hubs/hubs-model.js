5023764e534c0e4fc29abf2eb42d72fa85d2271f
Gabriel Cabrejas <gabriel@cabrejas.net>
2020-12-02 15:49:09 +0100

    change folder structure

--- api/hubs/hubs-model.js
+++ api/hubs/hubs-model.js
@@ -1,18 +1,19 @@
-const db = require('../data/dbConfig.js')
+const db = require('../../data/dbConfig.js')
 
 module.exports = {
   find,
   findById,
   add,
   remove,
-  update
+  update,
+  findHubMessages
 };
 
 function find(query) {
   let { page = 1, limit = 5, sortby = 'id', sortdir = 'asc' } = query;
   const offset = limit * (page - 1);
 
-  let rows = db('messages')
+  let rows = db('hubs')
     .orderBy(sortby, sortdir)
     .limit(limit)
     .offset(offset);
@@ -21,25 +22,32 @@ function find(query) {
 }
 
 function findById(id) {
-  return db('messages')
+  return db('hubs')
     .where({ id })
     .first();
 }
 
-async function add(message) {
-  const [id] = await db('messages').insert(message);
+async function add(hub) {
+  const [id] = await db('hubs').insert(hub);
 
   return findById(id);
 }
 
 function remove(id) {
-  return db('messages')
+  return db('hubs')
     .where({ id })
     .del();
 }
 
 function update(id, changes) {
-  return db('messages')
+  return db('hubs')
     .where({ id })
     .update(changes, '*');
+}
+
+function findHubMessages(hubId) {
+  return db('messages as m')
+    .join('hubs as h', 'm.hub_id', 'h.id')
+    .select('m.id', 'm.text', 'm.sender', 'h.id as hubId', 'h.name as hub')
+    .where({ hub_id: hubId });
 }
