62529316374f782f0ea52a3af6a9498821bf5d7d
Gabriel Cabrejas <gabriel@cabrejas.net>
2020-12-02 16:06:43 +0100

    clean up

--- api/hubs/hubs-router.js
+++ api/hubs/hubs-router.js
@@ -5,7 +5,6 @@ const Messages = require('../messages/messages-model.js');
 
 const router = express.Router();
 
-// this only runs if the url has /api/hubs in it
 router.get('/', (req, res) => {
   Hubs.find(req.query)
     .then(hubs => {
@@ -19,8 +18,6 @@ router.get('/', (req, res) => {
       });
     });
 });
-
-// /api/hubs/:id
 
 router.get('/:id', (req, res) => {
   Hubs.findById(req.params.id)
@@ -90,8 +87,6 @@ router.put('/:id', (req, res) => {
     });
 });
 
-// add an endpoint that returns all the messages for a hub
-// this is a sub-route or sub-resource
 router.get('/:id/messages', (req, res) => {
   Hubs.findHubMessages(req.params.id)
     .then(messages => {
@@ -106,7 +101,6 @@ router.get('/:id/messages', (req, res) => {
     });
 });
 
-// add an endpoint for adding new message to a hub
 router.post('/:id/messages', (req, res) => {
   const messageInfo = { ...req.body, hub_id: req.params.id };
 
