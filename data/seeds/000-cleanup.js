a661039bca80ad59422d9b43c76285f963ccdb7b
Gabriel Cabrejas <gabriel@cabrejas.net>
2021-04-07 17:21:50 +0200

    add script to reset db

--- data/seeds/000-cleanup.js
+++ data/seeds/000-cleanup.js
@@ -3,7 +3,6 @@ const cleaner = require('knex-cleaner');
 exports.seed = function(knex) {
   return cleaner.clean(knex, {
     mode: 'truncate',
-    restartIdentity: true,
     ignoreTables: ['knex_migrations', 'knex_migrations_lock'],
   });
 };
