d7133e7d5934ee030f6598fd3443b9ccefaa9206
Ladrillo <gabriel@cabrejas.net>
2021-11-02 15:09:31 +0100

    brand

--- api/server.js
+++ api/server.js
@@ -10,8 +10,8 @@ server.use('/api/hubs', hubsRouter);
 
 server.get('/', (req, res) => {
   res.send(`
-    <h2>Lambda Hubs API</h2>
-    <p>Welcome to the Lambda Hubs API</p>
+    <h2>Hubs API</h2>
+    <p>Welcome to the Hubs API</p>
   `);
 });
 
