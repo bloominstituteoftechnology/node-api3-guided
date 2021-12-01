de8ad6285f008df572e3dedf2aa6f7136e70da3b
Ladrillo <gabriel@cabrejas.net>
2021-12-01 18:58:38 +0100

    committing to lecture

--- api/hubs/hubs-middleware.js
+++ api/hubs/hubs-middleware.js
@@ -27,7 +27,7 @@ function maybeShortCircuit(req, res, next) {
   }
 }
 
-function errorHandling(err, req, res, next) { 
+function errorHandling(err, req, res, next) { // eslint-disable-line
   res.status(err.status || 500).json({
     message: `Horror in the router: ${err.message}`,
     stack: err.stack,
@@ -38,4 +38,5 @@ module.exports = {
   sayHi,
   greetCohort,
   maybeShortCircuit,
+  errorHandling,
 };
