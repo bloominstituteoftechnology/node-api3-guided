b14030d32e21b4d838006bea1a5dda37a2d622ce
Ladrillo <gabriel@cabrejas.net>
2021-12-01 18:19:06 +0100

    committing to lecture

--- api/hubs/hubs-middleware.js
+++ api/hubs/hubs-middleware.js
@@ -1,8 +1,10 @@
 function sayHi(req, res, next) {
   // 3 POSSIBLE OUTCOMES
+
   // 1- we allow the req to proceed
   // 2- we respond to the client
   // 3- we toss some error (to an error handling middl)
+  console.log(req.method);
 }
 
 
