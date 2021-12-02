c11061dad45153a50a76056d1d3414233f8006b9
Ladrillo <gabriel@cabrejas.net>
2021-12-02 13:26:49 +0100

    committing to lecture

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ const server = require('./api/server.js');
 const port = 9000;
 
 server.listen(port, () => {
-  console.log(`\n*** Server Running on http://localhost:${port} ***\n`);
+  console.log(`\n*** Server Running on http://localhost:${port} ***\n`); 
 });
