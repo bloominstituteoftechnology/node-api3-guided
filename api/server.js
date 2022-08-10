6b197964e9091d501d076f4ef76a3bfd398899da
James Blacklock <jamesblacklock@protonmail.com>
2022-08-10 12:18:35 -0400

    committing to "lecture"

--- api/server.js
+++ api/server.js
@@ -7,9 +7,9 @@ const server = express();
 server.use(express.json());
 
 
-server.use((req, res) => {
-  res.send('middleware!');
-});
+// server.use((req, res) => {
+//   res.send('middleware!');
+// });
 
 
 server.use('/api/hubs', hubsRouter);
